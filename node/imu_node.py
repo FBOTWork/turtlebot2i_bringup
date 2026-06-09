1e5f7ede1dec1543c7554fc20999cdc45f04b165
Henrivaz <henriquevaz03@gmail.com>
2026-06-08 21:53:17 -0300

    :construction: ekf yaml more clean, serial port change for hokuyoand imu

--- node/imu_node.py
+++ node/imu_node.py
@@ -14,7 +14,7 @@ class IMUNode:
 
         rospy.init_node('imu_node')
 
-        port = rospy.get_param('~port', '/dev/ttyUSB1')
+        port = rospy.get_param('~port', '/dev/ttyACM0')
         baud = rospy.get_param('~baud', 115200)
         self.frame_id = rospy.get_param('~frame_id', 'imu_link')
 
